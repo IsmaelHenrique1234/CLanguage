2b72368008e9a83abee87b6072215574403da38e
IsmaelVieiraDEV <ismaelvieira3000@gmail.com>
2025-10-20 13:36:16 -0300

    Refactor VetoresISMAEL.cpp for clarity and structure

--- VetoresISMAEL.cpp
+++ VetoresISMAEL.cpp
@@ -4,11 +4,11 @@ int main() {
     int select;
     setlocale(LC_ALL, "");
     do {
-    printf("Lista de Exercícios - Vetores - 9 questões - ISMAEL HENRIQUE VIEIRA TABORDA\nDigite de 1 á 9 para os Exercícios (0 para sair): ");
+    printf("Lista de ExercÃ­cios - Vetores - 9 questÃµes - ISMAEL\nDigite de 1 Ã¡ 9 para os ExercÃ­cios (0 para sair): ");
     scanf("%i", &select);
    
     switch (select) {
-      case 1: {  //1.Faça um programa que leia 10 valores reais e os apresente na ordem inversa entrada.
+      case 1: {  //1.FaÃ§a um programa que leia 10 valores reais e os apresente na ordem inversa entrada.
        float vetor[10];
        printf("\n");
        for (int i=0; i < 10; i++) {
@@ -20,7 +20,7 @@ int main() {
        }
         break;
       }
-      case 2: { //2.Faça um programa que leia 5 valores inteiros, armazeno-os em um vetor, calcule e apresente a soma destes valores.
+      case 2: { //2.FaÃ§a um programa que leia 5 valores inteiros, armazeno-os em um vetor, calcule e apresente a soma destes valores.
           int vet[5];
           int soma=0;
           printf("\n");
@@ -35,7 +35,7 @@ int main() {
           printf(" = %i", soma);
           break;
       }
-      case 3: { //3. Faça um algoritmo que leia um vetor N[20]. A seguir, encontre o menor elemento do vetor N e a sua posição dentro do vetor, mostrando: “O menor elemento de N é”, M, “e sua posição dentro do vetor é:”, P. 
+      case 3: { //3. FaÃ§a um algoritmo que leia um vetor N[20]. A seguir, encontre o menor elemento do vetor N e a sua posiÃ§Ã£o dentro do vetor, mostrando: â€œO menor elemento de N Ã©â€, M, â€œe sua posiÃ§Ã£o dentro do vetor Ã©:â€, P. 
           int N[20], Maior, Menor, i, p1, p2;
           printf("\n");
           for (i = 0; i < 20; i++) {
@@ -58,10 +58,10 @@ int main() {
           	p2 = i;
 		  }
 	      }
-		  printf("\nO Menor Valor: %i | Sua Posição: %i\nO Maior Valor: %i | Sua Posição: %i\n\n", Menor, p1, Maior, p2);
+		  printf("\nO Menor Valor: %i | Sua PosiÃ§Ã£o: %i\nO Maior Valor: %i | Sua PosiÃ§Ã£o: %i\n\n", Menor, p1, Maior, p2);
           break;
       }
-      case 4: { //4. Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
+      case 4: { //4. Escreva um algoritmo que leia dois vetores de 10 posiÃ§Ãµes e faÃ§a a multiplicaÃ§Ã£o dos elementos de mesmo Ã­ndice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
 	float vet1[10], vet2[10], vet3[10];
 	int i, j, k;
 	printf("\n");
@@ -81,7 +81,7 @@ int main() {
 	}
 		break;
 	  }
-	  case 5: { //5. Faça um algoritmo que leia um vetor S[20] e uma variável A. A seguir, mostre o produto da variável A pelo vetor.
+	  case 5: { //5. FaÃ§a um algoritmo que leia um vetor S[20] e uma variÃ¡vel A. A seguir, mostre o produto da variÃ¡vel A pelo vetor.
 	  	float S[20], A, Produto;
 	  	int i, j;
 	  	printf("\n");
@@ -99,7 +99,7 @@ int main() {
 		printf("\n");
 		break;
 	  }
-	  case 6: { //6. Escreva um algoritmo que leia e mostre um vetor de 20 números. A seguir, conte quantos valores pares existem no vetor.
+	  case 6: { //6. Escreva um algoritmo que leia e mostre um vetor de 20 nÃºmeros. A seguir, conte quantos valores pares existem no vetor.
 	    int vet[20], i, j, k, par;
 	    printf("\n");
 	    for (i = 0; i < 20; i++) {
@@ -119,7 +119,7 @@ int main() {
 		printf("tem %i Par(es) no vetor", par);
 		break;
 		}
-	   case 7: { //Escreva um algoritmo que leia um vetor de 20 posições e mostre-o. Em seguida, troque o primeiro elemento com o último, o segundo com o penúltimo, o terceiro com o antepenúltimo, e assim sucessivamente. Mostre o novo vetor depois da troca.
+	   case 7: { //Escreva um algoritmo que leia um vetor de 20 posiÃ§Ãµes e mostre-o. Em seguida, troque o primeiro elemento com o Ãºltimo, o segundo com o penÃºltimo, o terceiro com o antepenÃºltimo, e assim sucessivamente. Mostre o novo vetor depois da troca.
 	   	int vet[20], i, j, k;
 	   	printf("\n");
 	   	for (i = 0; i < 20; i++) {
@@ -139,17 +139,17 @@ int main() {
 		}
 		break;
 	   }
-	   case 8: { /*Faça um programa que leia 10 números inteiros, armazene-os em um vetor, solicite um valor de referência inteiro e:
-	    a. Imprima os números do vetor que são maiores que o valor referência.
-		b. Retorne quantos números armazenados no vetor são menores que o valor de referência.
-		c.Retorne quantas vezes o valor de referência aparece no vetor.*/
+	   case 8: { /*FaÃ§a um programa que leia 10 nÃºmeros inteiros, armazene-os em um vetor, solicite um valor de referÃªncia inteiro e:
+	    a. Imprima os nÃºmeros do vetor que sÃ£o maiores que o valor referÃªncia.
+		b. Retorne quantos nÃºmeros armazenados no vetor sÃ£o menores que o valor de referÃªncia.
+		c.Retorne quantas vezes o valor de referÃªncia aparece no vetor.*/
 	   	int vet[10], REFERENCE, i, j, k, MENOR = 0, IGUAL = 0;
 	   	printf("\n");
 	   	for (i = 0; i < 10; i++) {
 	   		printf("Ler valor inteiro [%i]: ", i);
 	   		scanf("%i", &vet[i]);
 		   }
-		printf("\nLer o valor de referência: ");
+		printf("\nLer o valor de referÃªncia: ");
 		scanf("%i", &REFERENCE);
 	   	for (j = 0; j < 10; j++) {
 	   		if (vet[j] < REFERENCE) {
@@ -162,13 +162,13 @@ int main() {
 		printf("\n");
 		for (k = 0; k < 10; k++) {
 			if (vet[k] > REFERENCE) {
-				printf("Numero maior que a referência no vetor [%i]: %i\n", k, vet[k]);
+				printf("Numero maior que a referÃªncia no vetor [%i]: %i\n", k, vet[k]);
 			}
 		}
-		printf("\nQuantidade de Números menores que a Referência no vetor: %i\nQuantidade de Números iguais à Referência no vetor: %i\n", MENOR, IGUAL);
+		printf("\nQuantidade de NÃºmeros menores que a ReferÃªncia no vetor: %i\nQuantidade de NÃºmeros iguais Ã  ReferÃªncia no vetor: %i\n", MENOR, IGUAL);
 		break;
 	   }
-	   case 9: { //Faça um programa que leia e armazene dois vetores a e b com 5 elementos cada e apresente o resultado de: somatoria de 5 elementos A[i] + B[4-i]
+	   case 9: { //FaÃ§a um programa que leia e armazene dois vetores a e b com 5 elementos cada e apresente o resultado de: somatoria de 5 elementos A[i] + B[4-i]
 	   	int A[5], B[5], i, j, k, Somatoria;
 	   	printf("\n");
 	   	for (k = 0; k < 5; k++) {
@@ -191,3 +191,4 @@ int main() {
 	  while (select != 0);
 	  return 0;
     }
+
